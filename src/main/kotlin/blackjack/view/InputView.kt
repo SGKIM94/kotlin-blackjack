c0114c33c57482f3f0c33964ebf66fbd6dfbe1c7
sgkim94 <tkdrn8578@naver.com>
2021-08-26 22:13:01 +0900

    feat: 배팅 금액을 받는 inputview 추가

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -8,6 +8,10 @@ object InputView {
         return question("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
     }
 
+    fun bettingMoney(name: String): Int {
+        return question("${name}의 배팅 금액은?").toInt()
+    }
+
     fun askIfPlayerWantToMoreCard(name: String): Boolean {
         val answer = question("$name 는 한장의 카드를 더 받겠습니까?(예는 $CONTINUE, 아니요는 $BREAK)")
 
